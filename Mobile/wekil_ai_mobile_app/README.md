afdd1e67a1f0ce4796ef3c15a3068dbdf48b0865
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-08-26 13:49:47 +0300

    mobile initialization

---
+++ Mobile/wekil_ai_mobile_app/README.md
@@ -0,0 +1,3 @@
+# wekil_ai_mobile_app
+
+A new Flutter project.
---
+++ Mobile/wekil_ai_mobile_app/android/app/src/main/kotlin/com/example/wekil_ai_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wekil_ai_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()

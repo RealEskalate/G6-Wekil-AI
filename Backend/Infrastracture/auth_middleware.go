86c8882a2db38672b219dbf6c3f839d471a53a79
yohannaaaa <yohannamerawi@gmail.com>
2025-08-26 10:09:13 +0300

    role updated to account type

--- Backend/Infrastracture/auth_middleware.go
+++ Backend/Infrastracture/auth_middleware.go
@@ -37,10 +37,9 @@ func (a *AuthMiddleware) JWTAuthMiddleware() gin.HandlerFunc {
             return
         }
 
-        // Store claims in Gin context
-        c.Set("user_id", claims.UserID) // ObjectID → string
+        c.Set("user_id", claims.UserID) 
         c.Set("email", claims.Email)
-        c.Set("role", claims.Role)
+        c.Set("account_type", claims.AccountType)
         c.Set("is_verified", claims.IsVerified)
 
         c.Next()
@@ -49,7 +48,7 @@ func (a *AuthMiddleware) JWTAuthMiddleware() gin.HandlerFunc {
 
 func RoleMiddleware(requiredRole string) gin.HandlerFunc {
     return func(c *gin.Context) {
-        role, exists := c.Get("role")
+        role, exists := c.Get("account_type")
         if !exists || role != requiredRole {
             c.JSON(http.StatusForbidden, gin.H{"error": "Forbidden: insufficient privileges"})
             c.Abort()

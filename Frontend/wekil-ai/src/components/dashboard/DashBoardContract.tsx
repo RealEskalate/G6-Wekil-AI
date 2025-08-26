e83e41e40745a1d9081ece9a15bfd73d73af41c1
nahomhailu12 <hailunahom47@gmail.com>
2025-08-26 23:29:02 +0300

    dashboard is now implemnted

---
+++ Frontend/wekil-ai/src/components/dashboard/DashBoardContract.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import {
+  Handshake,
+  Banknote,
+  ScrollText,
+  Shield,
+  Download,
+  Eye,
+  Edit3,
+} from "lucide-react";
+
+const contractTypes = {
+  service: {
+    icon: (
+      <Handshake className="w-16 h-12 mt-8 rounded-full text-blue-950 inline px-2" />
+    ),
+  },
+  sale: {
+    icon: (
+      <Banknote className="w-16 h-12 mt-8 rounded-full text-blue-950 inline px-2" />
+    ),
+  },
+  loan: {
+    icon: (
+      <ScrollText className="w-16 h-12 mt-8 rounded-full text-blue-950 inline px-2" />
+    ),
+  },
+  nonDisclosure: {
+    icon: (
+      <Shield className="w-16 h-12 mt-8 rounded-full text-blue-950 inline px-2" />
+    ),
+  },
+};
+
+const options = [
+  {
+    label: "show",
+    icon: <Eye className="inline mx-2 w-4 h-4 rounded-full  text-blue-400 " />,
+  },
+  {
+    label: "edit",
+    icon: (
+      <Edit3 className="inline mx-2 w-4 h-4 rounded-full  text-blue-400 " />
+    ),
+  },
+  {
+    label: "Export",
+    icon: (
+      <Download className="inline mx-2 w-4 h-4 rounded-full  text-blue-400 " />
+    ),
+  },
+];
+
+export interface ContractFormat {
+  type: "service" | "loan" | "sale" | "nonDisclosure";
+  title: string;
+  party1: string;
+  party2: string;
+  payment: string;
+  date: string;
+  status: string;
+  language: string;
+}
+
+interface DashBoardContractProps {
+  contract: ContractFormat;
+}
+
+export const DashBoardContract: React.FC<DashBoardContractProps> = ({
+  contract,
+}) => {
+  return (
+    <div className="mx-4 my-4 md:p-4 bg-white border border-gray-200 rounded-2xl shadow-2xl shadow-gray-200 grid grid-cols-8 gap-3">
+      <div className="col-span-1 flex align-middle">
+        {contractTypes[contract.type].icon}
+      </div>
+      <div className="col-span-7 md:col-span-4 pl-2">
+        <p className="text-blue-950 font-semibold my-2 text-lg">
+          {contract.title}
+        </p>
+        <p className="text-gray-500 mb-2 text-sm font-bold ml-4">{`${contract.party1} <-> ${contract.party2} . ${contract.payment} ETB`}</p>
+        <p className="text-blue-950 font-semibold mb-2 text-sm">
+          {contract.date}
+        </p>
+        <div className="flex justify-around">
+          <p
+            className={`inline px-2 py-1 rounded-full text-sm text-blue-950 font-semibold mb-2 ${
+              contract.status === "completed"
+                ? "bg-blue-950 text-white"
+                : "bg-white text-blue-950 border border-gray-200"
+            }`}
+          >
+            {contract.status}
+          </p>
+          <p className="inline mx-2 text-blue-950 font-semibold border rounded-full px-4 border-gray-100 my-2">
+            {contract.language === "en" ? "EN" : "AM"}
+          </p>
+        </div>
+      </div>
+      <div className="col-span-8 md:col-span-3 flex items-center mb-4">
+        {options.map((item, index) => (
+          <div
+            className="inline mx-2 text-gray-700 hover:text-blue-400 bg-gray-100 font-semibold border border-gray-200 shadow-5xl shadow-gray-500 cursor-pointer px-3 py-1 rounded-full"
+            key={index}
+          >
+            {item.icon}
+            {item.label}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DashBoardContract;
---
+++ Frontend/wekil-ai/src/components/dashboard/dashboardCard.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode } from "react";
+import { FileText, Plus } from "lucide-react";
+
+const DashboardCard = ({
+  title,
+  children,
+  type,
+}: {
+  title: string;
+  children: ReactNode;
+  type: "create" | "view";
+}) => {
+  return (
+    <div className="border w-9/10 bg-white  border-gray-200 shadow-md shadow-gray-200 hover:shadow-blue-200 rounded-3xl p-4 hover:shadow-xl cursor-pointer col-span-2 md:col-span-1">
+      <div className="flex justify-center">
+        {type === "create" ? (
+          <Plus className="w-1/5 h-1/5 hover:w-1/4 hover:h-1/4 transition-all rounded-full bg-blue-400" />
+        ) : (
+          <FileText className="w-1/5 h-1/5 hover:w-1/4 hover:h-1/4 transition-all text-blue-500 " />
+        )}
+      </div>
+      <p className="text-center my-6 text-xl text-blue-950 font-bold">
+        {title}
+      </p>
+      <p className="text-center my-6 text-md text-gray-600">{children}</p>
+      {type === "view" ? (
+        <div className="flex justify-center">
+          <div className="border p-2 font-bold text-blue-600 border-blue-800 rounded-full">
+            3 Contracts
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default DashboardCard;

278c4d55e242d96655113d53e5f43a026c9785ed
kenan <457kenanmengistu@gmail.com>
2025-08-27 14:18:49 +0300

    'add sidebar'

--- Frontend/wekil-ai/src/app/dashboard/page.tsx
+++ Frontend/wekil-ai/src/app/dashboard/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { Sidebar } from "../../components/Sidebar";
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex h-screen bg-gray-50">
+      <Sidebar />
+    </div>
+  );
 };
 
 export default page;

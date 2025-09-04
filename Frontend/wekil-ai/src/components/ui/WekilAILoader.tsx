840f7514ca198b490af52dc3636215f4fbcc8182
anteneh83 <antenehgetnet83@gmail.com>
2025-09-04 05:03:50 -0900

    add loader for admin, and dashboard pages

---
+++ Frontend/wekil-ai/src/components/ui/WekilAILoader.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function WeKilAILoader() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+      <Image
+        src="/loader.gif"
+        alt="Loading..."
+        width={200} // increase size
+        height={200} // increase size
+        className="object-contain"
+      />
+    </div>
+  );
+}
